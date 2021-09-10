1317f1fd5e3e75b725ea31e32c431f4c5ecbe36c
EnderKingJ#0001 <78042486+EnderKingJ@users.noreply.github.com>
2021-09-10 16:06:22 +0000

    lots of fixes, errors for no reason

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http')
 const https = require('https')
 const mime = require('mime')
 const fs = require('fs')
-var proxify = require('./lib/proxy')
 btoa = str => new Buffer.from(str).toString('base64'),
 atob = str => new Buffer.from(str, 'base64').toString('utf-8')
 var { prefix, port, blockedHosts } = require('./config.json')
@@ -14,6 +13,8 @@ function getMimeType(url) {
     }
     return mime.getType(url) || 'text/html';
 };
+
+var proxy = new (require('./lib/smoke'))(prefix, {blockedHosts})
 
 app = (req, res) => {
   if (req.url.startsWith('/service')) {
@@ -28,7 +29,7 @@ app = (req, res) => {
     res.writeHead(200, {'content-type': 'application/javascript'})
     res.end(fs.readFileSync('./lib/smoke.js'))
   } else if (req.url.startsWith(prefix)) {
-    var url = 'https://'+req.url.replace(prefix, '')
+    /*var url = 'https://'+req.url.replace(prefix, '')
     var urlSlice = new URL(url)
     blockedHosts.map((data) => {
       if (new URL(data.url).host == urlSlice.host) {
@@ -36,19 +37,8 @@ app = (req, res) => {
         res.end(fs.readFileSync('./lib/err.html', 'utf-8').replace('err_reason', 'Hostname '+data.url+' Blocked: '+(data.reason ? data.reason : 'Hostname on Blocklist. Please ask the Proxy Maintainer for Details')))
         return
       }
-    })
-    if (!url.replace('https://', '').split('/').length > 2) {
-      url = url.slice(0, -1);
-    }
-    var mime_type = getMimeType(url).replace('application/vnd.lotus-organizer', 'text/html')
-    axios.get(url, { responseType: 'arraybuffer'  })
-        .then(({ data }) => {
-            data = proxify.request(data, url)
-            res.writeHead(200, {'content-type': mime_type});
-            res.end(data);
-        }).catch(error => {
-          res.writeHead(200, {'content-type': 'text/html'}).end(fs.readFileSync('./lib/err.html', 'utf-8').replace('err_reason', 'Error: "'+error+'"'))
-        })
+    })*/
+    return proxy.request(req, res);
   } else if (req.url==='/') {
     res.writeHead(200, {'content-type': 'text/html'}).end(fs.readFileSync('./public/index.html'))
   } else {
